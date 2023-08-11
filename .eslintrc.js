bf42483dc95696f682bac956509085f76a993588
lekan.dev <realjoy9@gmail.com>
2023-08-11 06:43:14 +0100

    more linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,52 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "extends": [
-        "next",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+    'extends': [
+        'next',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended'
     ],
-      "overrides": [
-        {
-          files: ['**/*.ts', '**/*.tsx'], 
-          parser: '@typescript-eslint/parser',
-          parserOptions: {
-            project: './tsconfig.json',
-          },
+    'overrides': [
+      {
+        files: ['**/*.ts', '**/*.tsx'], 
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          project: './tsconfig.json',
         },
-        {
-          env: {
-            node: true,
-          },
-          files: [
-            '.eslintrc.{js,cjs}',
-          ],
-          parserOptions: {
-            sourceType: 'script',
-          },
+      },
+      {
+        env: {
+          node: true,
         },
-      ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "projext": './tsconfig.json',
+        files: [
+          '.eslintrc.{js,cjs}',
+        ],
+        parserOptions: {
+          sourceType: 'script',
+        },
+      },
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
+        'projext': './tsconfig.json',
     },
-    "plugins": [
-        "react"
+    'plugins': [
+        'react'
     ],
-    "rules": {
+    'rules': {
         // no unused variable
-        "@typescript-eslint/no-unused-vars": "error",
+        '@typescript-eslint/no-unused-vars': 'error',
         // no unused variable
-        "@typescript-eslint/no-unused-expressions": "error",
+        '@typescript-eslint/no-unused-expressions': 'error',
         // react in jsx / tsx off
-        "react/react-in-jsx-scope": "off",
+        'react/react-in-jsx-scope': 'off',
+        'quotes': ['error', 'single'],
+        'jsx-quotes': ['error', 'prefer-double'],
+        'semi': ['error', 'always'],
+
 
     }
-}
+};
